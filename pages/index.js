684e146290ffdd4c20d471f208d84dba6a0d72f4
Evandir Baldanza <evandir.baldanza@gmail.com>
2022-01-18 14:06:50 -0300

    primeira pagina 1

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,12 @@
-import { useState} from 'react';
-
+import Link from 'next/link'
 function Home() {
     return (
         <div>
-            <h1>Home 1233</h1>
-            <Contador />
-        </div>
-        
-    )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
+            <h1>Home</h1>
+            <Link href="/sobre">
+                <a>Acessar Página Sobre</a>
+            </Link>
         </div>
     )
 }
-
-function Pagina() {
-    return <Contador />
-}
-
-export default Home
+export default Home;
